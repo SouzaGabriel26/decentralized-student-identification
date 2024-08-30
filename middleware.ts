c87bd81cffb54e6d50805c8e7d3298996951963d
SouzaGabriel26 <gabriel.asouza2608@hotmail.com>
2024-08-30 19:48:46 -0300

    feat: creates `middleware.ts` to handle public/private routes

---
+++ middleware.ts
@@ -0,0 +1,27 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+
+  if (publicRoutes.includes(pathname)) {
+    return NextResponse.next();
+  }
+
+  // TODO: verify JWT token
+  console.log('Private route...');
+  return NextResponse.redirect(new URL('/', request.url));
+}
+
+const publicRoutes: Array<string> = ['/', '/register'];
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico, sitemap.xml, robots.txt (metadata files)
+     */
+    '/((?!_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)',
+  ],
+};
