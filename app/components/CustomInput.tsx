bdc8d5582f51046ed88f510bf70fdf2bd9741a2e
SouzaGabriel26 <gabriel.asouza2608@hotmail.com>
2024-09-04 22:47:40 -0300

    feat: creates custom `input` component

---
+++ app/components/CustomInput.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import { FormControl, TextInput, TextInputProps } from '@primer/react';
+
+type CustomInputProps = TextInputProps & {
+  name: string;
+  label: string;
+};
+
+export function CustomInput({
+  name,
+  label,
+  id,
+  required,
+  ...props
+}: CustomInputProps) {
+  const inputId = id || name;
+
+  console.log({ required });
+
+  return (
+    <FormControl
+      id={inputId}
+      sx={{
+        width: '100%',
+      }}
+      required
+    >
+      <FormControl.Label>{label}</FormControl.Label>
+      <TextInput name={name} block {...props} />
+    </FormControl>
+  );
+}
