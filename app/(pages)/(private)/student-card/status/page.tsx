a185938d6fbae91930ec16f85598fd76ec601134
SouzaGabriel26 <gabriel.asouza2608@hotmail.com>
2024-09-18 17:21:53 -0300

    feat: implements `/student-card/status` page

--- app/(pages)/(private)/student-card/status/page.tsx
+++ app/(pages)/(private)/student-card/status/page.tsx
@@ -1,3 +1,39 @@
-export default function Page() {
-  return <p>Status da carteira de estudante</p>;
+import { identity } from '@/utils/idendity';
+import { Box, Label, Text } from '@primer/react';
+import { redirect, RedirectType } from 'next/navigation';
+
+export default async function Page() {
+  const signedUser = await identity.isLoggedIn();
+  if (!signedUser) return redirect('/', RedirectType.replace);
+
+  let statusObject = {
+    APPROVED: 'Aprovado',
+    REJECTED: 'Rejeitado',
+    PENDING: 'Pendente',
+  };
+
+  return (
+    <Box
+      sx={{
+        height: '100%',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        gap: 2,
+      }}
+    >
+      <Text>Status da carteira:</Text>
+      <Label
+        variant={
+          signedUser.status === 'APPROVED'
+            ? 'success'
+            : signedUser.status === 'REJECTED'
+              ? 'danger'
+              : 'attention'
+        }
+      >
+        {statusObject[signedUser.status]}
+      </Label>
+    </Box>
+  );
 }
