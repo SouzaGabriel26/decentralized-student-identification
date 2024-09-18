58d3a7a7ae369f817537dd66e452b9596c9465ea
SouzaGabriel26 <gabriel.asouza2608@hotmail.com>
2024-09-18 17:21:10 -0300

    feat: implements `/student-card` page when userCard is not approved yet

--- app/(pages)/(private)/student-card/page.tsx
+++ app/(pages)/(private)/student-card/page.tsx
@@ -1,3 +1,26 @@
-export default function Page() {
-  return <p>Carteira de estudante</p>;
+import { identity } from '@/utils/idendity';
+import { Box, Text } from '@primer/react';
+import Link from 'next/link';
+import { redirect, RedirectType } from 'next/navigation';
+
+export default async function Page() {
+  const signedUser = await identity.isLoggedIn();
+  if (!signedUser) return redirect('/', RedirectType.replace);
+
+  if (signedUser.status !== 'APPROVED') {
+    return (
+      <Box
+        sx={{
+          height: '100%',
+          display: 'flex',
+          gap: 2,
+        }}
+      >
+        <Text>Carteira de estudante não disponível.</Text>
+        <Link href="/student-card/status">Ver status</Link>
+      </Box>
+    );
+  }
+
+  return <p>Carteira de estudante descriptografada</p>;
 }
