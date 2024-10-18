a8bc4ee3f5c25c4cf6069b5817afc1d7c4dbe5fb
SouzaGabriel26 <gabriel.asouza2608@hotmail.com>
2024-10-17 21:47:42 -0300

    refactor(`middleware`): removes logic of set userId in response headers making it simpler

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,6 @@
 import { constants } from '@/utils/constants';
 import { navItems } from '@/utils/navItems';
 import { jwtVerify } from 'jose';
-import { cookies } from 'next/headers';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function middleware(request: NextRequest) {
@@ -10,36 +9,19 @@ export async function middleware(request: NextRequest) {
   const accessToken = request.cookies.get(constants.access_token_key)?.value;
   const response = NextResponse.next();
 
-  const privatePages = navItems
-    .filter((item) => item.auth.isPrivate)
-    .map((item) => item.href as string);
-
-  if (!accessToken && privatePages.includes(pathname)) {
-    return NextResponse.redirect(new URL('/', request.url));
+  if (!accessToken && isPrivatePage(pathname)) {
+    return redirectToHomePage(request);
   }
 
   if (accessToken) {
+    if (isOnlyNotSignedInPage(pathname)) {
+      return redirectToHomePage(request);
+    }
+
     try {
-      const { payload } = await jwtVerify(
-        accessToken,
-        new TextEncoder().encode(constants.jwt_secret),
-      );
+      const secret = new TextEncoder().encode(constants.jwt_secret);
 
-      const { sub: userId } = payload;
-      if (userId) {
-        response.headers.set(constants.user_id_header_key, userId);
-      }
-
-      const onlyNotSignedInPages = navItems
-        .filter((item) => item.auth.onlyNotSignedIn)
-        .map((item) => item.href as string);
-
-      if (onlyNotSignedInPages.includes(pathname) && userId) {
-        if (process.env.NODE_ENV === 'development') {
-          cookies().delete(constants.access_token_key);
-        }
-        return NextResponse.redirect(new URL('/', request.url));
-      }
+      await jwtVerify(accessToken, secret);
     } catch {
       response.cookies.delete(constants.access_token_key);
     }
@@ -53,3 +35,23 @@ export const config = {
     '/((?!_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)',
   ],
 };
+
+function redirectToHomePage(request: NextRequest) {
+  return NextResponse.redirect(new URL('/', request.url));
+}
+
+function isOnlyNotSignedInPage(pathname: string) {
+  const onlyNotSignedInPages = navItems
+    .filter((item) => item.auth.onlyNotSignedIn)
+    .map((item) => item.href as string);
+
+  return onlyNotSignedInPages.includes(pathname);
+}
+
+function isPrivatePage(pathname: string) {
+  const privatePages = navItems
+    .filter((item) => item.auth.isPrivate)
+    .map((item) => item.href as string);
+
+  return privatePages.includes(pathname);
+}
