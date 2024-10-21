9a275859d9338ecf7ea00007cf1dd0bad5d15f42
SouzaGabriel26 <gabriel.asouza2608@hotmail.com>
2024-10-20 21:13:22 -0300

    refactor: adds function to get the last pull request number

--- infra/script/deployment.ts
+++ infra/script/deployment.ts
@@ -3,7 +3,7 @@ import { promisify } from 'node:util';
 
 const exec = promisify(child_process_exec);
 
-deployment();
+//deployment();
 
 async function deployment() {
   const token = process.env.VERCEL_TOKEN;
@@ -31,11 +31,15 @@ async function deployment() {
 }
 
 async function createPullRequestComment(comment: string) {
-  const pullRequestNumber = process.env.STD_IDENTIFICATION_PULL_REQUEST_NUMBER;
+  const pullRequestNumber = await getPullRequestNumber();
+  if (!pullRequestNumber) {
+    return;
+  }
+
   const token = process.env.VERCEL_TOKEN;
 
   try {
-    await fetch(
+    const res = await fetch(
       `https://api.github.com/repos/souzagabriel26/student-identification/issues/${pullRequestNumber}/comments`,
       {
         method: 'POST',
@@ -49,7 +53,30 @@ async function createPullRequestComment(comment: string) {
         }),
       },
     );
+
+    console.log(await res.json());
   } catch {
     console.log('Error creating pull request comment');
+  }
+}
+
+async function getPullRequestNumber() {
+  try {
+    const response = await fetch(
+      'https://api.github.com/repos/souzagabriel26/student-identification/pulls?state=all&direction=desc',
+      {
+        method: 'GET',
+        headers: {
+          'X-GitHub-Api-Version': '2022-11-28',
+        },
+      },
+    );
+
+    const pullRequests = await response.json();
+    console.log(pullRequests[0].number);
+    return pullRequests[0].number as number;
+  } catch {
+    console.log('Error getting pull request number');
+    return null;
   }
 }
