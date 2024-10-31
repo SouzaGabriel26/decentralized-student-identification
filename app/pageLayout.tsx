c23661e8767499738cc658c26ee176ce3bde9978
SouzaGabriel26 <gabriel.asouza2608@hotmail.com>
2024-10-30 22:39:22 -0300

    feat: adds validations to check if `ADMIN` users have metamask installed and can interact

--- app/pageLayout.tsx
+++ app/pageLayout.tsx
@@ -12,6 +12,44 @@ type Props = {
 export async function PageLayout({ children }: Props) {
   const userSigned = await identity.getMe();
 
+  if (userSigned && userSigned.role === 'ADMIN') {
+    return (
+      <Web3Provider>
+        <Box
+          sx={{
+            bg: 'canvas.default',
+            minHeight: '100vh',
+            display: 'flex',
+            flexDirection: 'column',
+          }}
+        >
+          <Header />
+
+          <Box
+            sx={{
+              flex: 1,
+              display: 'flex',
+            }}
+          >
+            <Sidebar />
+
+            <Box
+              sx={{
+                px: 4,
+                py: 2,
+                width: '100%',
+                height: '90vh',
+                overflowY: 'auto',
+              }}
+            >
+              {children}
+            </Box>
+          </Box>
+        </Box>
+      </Web3Provider>
+    );
+  }
+
   return (
     <Box
       sx={{
@@ -40,11 +78,7 @@ export async function PageLayout({ children }: Props) {
             overflowY: 'auto',
           }}
         >
-          {userSigned && userSigned.role === 'ADMIN' ? (
-            <Web3Provider>{children}</Web3Provider>
-          ) : (
-            children
-          )}
+          {children}
         </Box>
       </Box>
     </Box>
